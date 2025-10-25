5aab556cf2f507aed12779fd8c02c253545d75bd
MiltonCarlosRibeiro <milton.carlos.dev@gmail.com>
2025-10-25 11:12:41 -0300

    m "docs: add Swagger UI (springdoc)

---
+++ src/test/java/com/esg/iot/smartenergy/SmartEnergyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esg.iot.smartenergy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartEnergyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
